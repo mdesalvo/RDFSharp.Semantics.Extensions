56fe56446757d91e1b55029a125d188027aad214
Marco De Salvo <marco.desalvo@vodafone.com>
2023-02-17 15:58:17 +0100

    Completed test for sf:MultiLineString

--- RDFSharp.Semantics.Extensions.GEO.Test/GEOOntologyLoaderTest.cs
+++ RDFSharp.Semantics.Extensions.GEO.Test/GEOOntologyLoaderTest.cs
@@ -66,6 +66,9 @@ namespace RDFSharp.Semantics.Extensions.GEO.Test
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanrome_mpt"), RDFVocabulary.RDF.TYPE, RDFVocabulary.GEOSPARQL.SF.MULTI_POINT));
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanrome_mpt"), RDFVocabulary.GEOSPARQL.AS_WKT, new RDFTypedLiteral("MULTIPOINT ((9.18854 45.464664), (12.496365 41.902782))", RDFModelEnums.RDFDatatypes.GEOSPARQL_WKT)));
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanrome_mpt"), RDFVocabulary.GEOSPARQL.AS_GML, new RDFTypedLiteral("<gml:MultiPoint xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:pointMember><gml:Point><gml:pos>9.18854 45.464664</gml:pos></gml:Point></gml:pointMember><gml:pointMember><gml:Point><gml:pos>12.496365 41.902782</gml:pos></gml:Point></gml:pointMember></gml:MultiPoint>", RDFModelEnums.RDFDatatypes.GEOSPARQL_GML)));
+            graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mls"), RDFVocabulary.RDF.TYPE, RDFVocabulary.GEOSPARQL.SF.MULTI_LINESTRING));
+            graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mls"), RDFVocabulary.GEOSPARQL.AS_WKT, new RDFTypedLiteral("MULTILINESTRING ((9.18854 45.464664, 12.496365 41.902782), (12.496365 41.902782, 14.2681244 40.8517746))", RDFModelEnums.RDFDatatypes.GEOSPARQL_WKT)));
+            graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mls"), RDFVocabulary.GEOSPARQL.AS_GML, new RDFTypedLiteral("<gml:MultiCurve xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:curveMember><gml:LineString><gml:posList>9.18854 45.464664 12.496365 41.902782</gml:posList></gml:LineString></gml:curveMember><gml:curveMember><gml:LineString><gml:posList>12.496365 41.902782 14.2681244 40.8517746</gml:posList></gml:LineString></gml:curveMember></gml:MultiCurve>", RDFModelEnums.RDFDatatypes.GEOSPARQL_GML)));
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mpl"), RDFVocabulary.RDF.TYPE, RDFVocabulary.GEOSPARQL.SF.MULTI_POLYGON));
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mpl"), RDFVocabulary.GEOSPARQL.AS_WKT, new RDFTypedLiteral("MULTIPOLYGON (((9.18854 45.464664, 12.496365 41.902782, 14.2681244 40.8517746, 9.18854 45.464664)), ((12.496365 41.902782, 14.2681244 40.8517746, 9.18854 45.464664, 12.496365 41.902782)))", RDFModelEnums.RDFDatatypes.GEOSPARQL_WKT)));
             graph.AddTriple(new RDFTriple(new RDFResource("ex:milanromenaples_mpl"), RDFVocabulary.GEOSPARQL.AS_GML, new RDFTypedLiteral("<gml:MultiSurface xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:surfaceMember><gml:Polygon><gml:exterior><gml:LinearRing><gml:posList>9.18854 45.464664 12.496365 41.902782 14.2681244 40.8517746 9.18854 45.464664</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></gml:surfaceMember><gml:surfaceMember><gml:Polygon><gml:exterior><gml:LinearRing><gml:posList>12.496365 41.902782 14.2681244 40.8517746 9.18854 45.464664 12.496365 41.902782</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></gml:surfaceMember></gml:MultiSurface>", RDFModelEnums.RDFDatatypes.GEOSPARQL_GML)));
@@ -78,8 +81,8 @@ namespace RDFSharp.Semantics.Extensions.GEO.Test
             Assert.IsTrue(geoOntology.URI.Equals(RDFNamespaceRegister.DefaultNamespace.NamespaceUri));
             Assert.IsTrue(geoOntology.Model.ClassModel.ClassesCount == 20);
             Assert.IsTrue(geoOntology.Model.PropertyModel.PropertiesCount == 36);
-            Assert.IsTrue(geoOntology.Data.IndividualsCount == 7);
-            Assert.IsTrue(geoOntology.Geometries.Count == 7);
+            Assert.IsTrue(geoOntology.Data.IndividualsCount == 8);
+            Assert.IsTrue(geoOntology.Geometries.Count == 8);
             Assert.IsTrue(geoOntology.Geometries.ContainsKey("ex:milan"));
             Assert.IsTrue(geoOntology.Geometries["ex:milan"].Item1.EqualsTopologically(new Point(9.18854, 45.464664)));
             Assert.IsTrue(geoOntology.Geometries["ex:milan"].Item2.EqualsTopologically(new Point(514739.23764243146, 5034588.076214247)));
@@ -100,6 +103,15 @@ namespace RDFSharp.Semantics.Extensions.GEO.Test
                 new Point(9.18854, 45.464664), new Point(12.496365, 41.902782) })));
             Assert.IsTrue(geoOntology.Geometries["ex:milanrome_mpt"].Item2.EqualsTopologically(new MultiPoint(new Point[] { 
                 new Point(514739.23764243146, 5034588.076214247), new Point(790020.659168055, 4644896.163254826) })));
+            Assert.IsTrue(geoOntology.Geometries.ContainsKey("ex:milanromenaples_mls"));
+            Assert.IsTrue(geoOntology.Geometries["ex:milanromenaples_mls"].Item1.EqualsTopologically(new MultiLineString(new LineString[] {
+                new LineString(new Coordinate[] { new Coordinate(9.18854, 45.464664), new Coordinate(12.496365, 41.902782) }),
+                new LineString(new Coordinate[] { new Coordinate(12.496365, 41.902782), new Coordinate(14.2681244, 40.8517746) })
+            })));
+            Assert.IsTrue(geoOntology.Geometries["ex:milanromenaples_mls"].Item2.EqualsTopologically(new MultiLineString(new LineString[] {
+                new LineString(new Coordinate[] { new Coordinate(514739.23764243146, 5034588.076214247), new Coordinate(790020.659168055, 4644896.163254826) }),
+                new LineString(new Coordinate[] { new Coordinate(790020.659168055, 4644896.163254826), new Coordinate(944139.3677008688, 4535678.997426257) })
+            })));
             Assert.IsTrue(geoOntology.Geometries.ContainsKey("ex:milanromenaples_mpl"));
             Assert.IsTrue(geoOntology.Geometries["ex:milanromenaples_mpl"].Item1.EqualsTopologically(new MultiPolygon(new Polygon[] { 
                 new Polygon(new LinearRing(new Coordinate[] { new Coordinate(9.18854, 45.464664), new Coordinate(12.496365, 41.902782), new Coordinate(14.2681244, 40.8517746), new Coordinate(9.18854, 45.464664) })),
