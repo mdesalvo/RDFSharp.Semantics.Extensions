200ca8b21024431f55f1a0f3cd44a2d5948eb80f
Marco De Salvo <floating_point_9@msn.com>
2023-02-22 19:52:42 +0100

    Implemented geof:sfEquals

--- RDFSharp.Semantics.Extensions.GEO/QueryExtensions/Expressions/Abstractions/GEOExpression.cs
+++ RDFSharp.Semantics.Extensions.GEO/QueryExtensions/Expressions/Abstractions/GEOExpression.cs
@@ -14,12 +14,10 @@
    limitations under the License.
 */
 
-using NetTopologySuite;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
 using NetTopologySuite.IO.GML2;
 using NetTopologySuite.IO.GML3;
-using ProjNet.CoordinateSystems;
 using RDFSharp.Model;
 using RDFSharp.Query;
 using System;
@@ -185,6 +183,11 @@ namespace RDFSharp.Semantics.Extensions.GEO
                         Geometry envelopeGeometryUTM = leftGeometryUTM.Envelope;
                         Geometry envelopeGeometryWGS84 = GEOConverter.GetWGS84GeometryFromUTM(envelopeGeometryUTM, utmFromWGS84LeftGeometry);
                         expressionResult = new RDFTypedLiteral(WKTWriter.Write(envelopeGeometryWGS84), RDFModelEnums.RDFDatatypes.GEOSPARQL_WKT);
+                    }
+                    else if (this is GEOEqualsExpression)
+                    {
+                        bool sfEquals = leftGeometryUTM.EqualsTopologically(rightGeometryUTM);
+                        expressionResult = sfEquals ? RDFTypedLiteral.True : RDFTypedLiteral.False;
                     }
                     else if (this is GEOGetSRIDExpression)
                     {
