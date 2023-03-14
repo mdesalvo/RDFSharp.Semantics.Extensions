be08294df10f023990f3e40804ae692962da9b72
Marco De Salvo <marco.desalvo@vodafone.com>
2023-03-14 10:14:19 +0100

    WIP empty classes for begin modeling the core of W3C TIME

---
+++ RDFSharp.Semantics.Extensions.TIME/Abstractions/TIMEInstant.cs
@@ -0,0 +1,43 @@
+﻿/*
+   Copyright 2012-2023 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System;
+
+namespace RDFSharp.Semantics.Extensions.TIME
+{
+    /// <summary>
+    /// TIMEInstant represents a temporal entity with zero extent or duration
+    /// </summary>
+    public class TIMEInstant : TIMEEntity
+    {
+        #region Properties
+        
+        #endregion
+
+        #region Ctors
+        
+        #endregion
+
+        #region Interfaces
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
